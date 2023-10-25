0f4dad56ba35218e44f0d4793babcd8cad7adfec
Murilll <murilosertorio25@gmail.com>
2023-10-25 10:02:34 -0300

    UPDATE: take position

--- TEST/Program.cs
+++ TEST/Program.cs
@@ -12,16 +12,35 @@
 // var fileData = File.ReadLines(archive!).FirstOrDefault();
 // System.Console.WriteLine(fileData);
 
+using System.ComponentModel;
 
-ulong num  = 65536;
-var res = num >>> (16 - 6) & (16 - 6);
+ulong num = 65536;
+num = 103_213_432_832;
+int totalLength = Convert.ToString((long)num, toBase: 2).Length;
+ulong table = 64;
+int width = 8;
+int column = 2 - 1;
+int row = 2 - 1;
+int objectivePosition = (width * column) + row;
+var resbin = num >>> objectivePosition;
+var objectiveIsOcupped = (resbin & (table - (ulong)objectivePosition + 1)) == 1;
 
-if(res == 0)
-    num += (1 << 10);
-res = num >>> (16 - 6) & ((16 - 6)+1);
+if (!objectiveIsOcupped)
+    num += (ulong)(1 << objectivePosition);
 
+// res = num >>> (16 - 6) & ((16 - 6) + 1);
+// for (int i = 0; i < objective-1; i++)
+// {
+//     res = res >>> 1;
+//     System.Console.WriteLine(Convert.ToString((long)res, toBase: 2));
+// }
 
+System.Console.Write("binario Num:  ");
 System.Console.WriteLine(Convert.ToString((long)num, toBase: 2));
+System.Console.Write("Decimal Num resultado:  ");
 System.Console.WriteLine(num);
 // System.Console.WriteLine(Convert.ToString((long)res, toBase: 2));
-System.Console.WriteLine(res);
+System.Console.Write("binario Res:");
+System.Console.WriteLine(Convert.ToString((long)resbin, toBase: 2));
+System.Console.Write("Ocupado:  ");
+System.Console.WriteLine(objectiveIsOcupped);
