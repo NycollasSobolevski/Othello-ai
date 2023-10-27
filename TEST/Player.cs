d350a1c01b46e056c30e0ec5b4866c0ea74b60f7
nycollassobolevski <sobolevski.academy@gmail.com>
2023-10-27 08:08:14 -0300

    UPDATE: player

--- TEST/Player.cs
+++ TEST/Player.cs
@@ -4,31 +4,95 @@ public class Player
 {
     public ulong CurrentPlays { get; set; }
     public Player Enemy { get; set; }
+    public List<ulong> PossibleMoves = new();
 
-    public ulong Play (int column, int row) {
+    public List<ulong> Plays (int column, int row) {
+        List<ulong> possibleResults = new() ;
         int table = 64;
         int width = (int)Math.Sqrt(table);
-        column --; 
+        column --;
         row --;
-        System.Console.WriteLine(column+row);
         int objectivePosition = (width * column) + row;
         ulong binResult = CurrentPlays >>> objectivePosition;
-        //TODO verificar canplay do enimigo e sua para ver se nao há prça de ambos
-        return CurrentPlays + (ulong)(1 << objectivePosition);;
+
+        bool top    = Enemy.HavePiece(column, row - 1);
+        bool bottom = Enemy.HavePiece(column, row + 1);
+        bool left   = Enemy.HavePiece(column-1, row);
+        bool right  = Enemy.HavePiece(column+1, row);
+        bool Diag1  = Enemy.HavePiece(column-1, row - 1);
+        bool Diag2  = Enemy.HavePiece(column+1, row - 1);
+        bool Diag3  = Enemy.HavePiece(column-1, row + 1);
+        bool Diag4  = Enemy.HavePiece(column+1, row + 1);
+
+        if (top)
+        {
+            int qtd = 0;
+            while(top){
+                top = Enemy.HavePiece(column, row -1 -qtd);
+                qtd++;
+            }
+            System.Console.WriteLine($"Top possibles move {qtd}");
+            // CurrentPlays + (ulong)(1 << objectivePosition) //movendo 1 
+            PossibleMoves.Add(CurrentPlays + (ulong)(1 << (width * column) + (row -qtd)));
+            
+        }
+        if (bottom)
+        {
+            int qtd = 0;
+            while(bottom){
+                bottom = Enemy.HavePiece(column, row +1 +qtd);
+                qtd++;
+            }
+            System.Console.WriteLine($"Bottom possibles move {qtd}");
+            ulong move = CurrentPlays + (ulong)(1 << (width * column) + row + qtd);
+            PossibleMoves.Add(move);
+        }
+        if (right)
+        {
+            int qtd = 0;
+            while(right){
+                right = Enemy.HavePiece(column +1 +qtd, row );
+                qtd++;
+            }
+            System.Console.WriteLine($"Right possibles move {qtd}");
+            PossibleMoves.Add(CurrentPlays + (ulong)(1 << (width * (column + 1)) + row));
+        }
+        if (left)
+        {
+            int qtd = 0;
+            while(left){
+                left = Enemy.HavePiece(column -1 -qtd, row );
+                qtd++;
+            }
+            System.Console.WriteLine($"Left possibles move {qtd}");
+            PossibleMoves.Add(CurrentPlays + (ulong)(1 << (width * (column - 1)) + row));
+        }
+        return possibleResults;
     } 
 
-    public bool CanPlay (int column, int row) {
+    public bool HavePiece (int column, int row) {
         int table = 64;
         int width = (int)Math.Sqrt(table);
-        column --; 
-        row --;
-        System.Console.WriteLine(column+row);
+
         int objectivePosition = (width * column) + row;
-        ulong binResult = CurrentPlays >>> objectivePosition;
-        bool objectiveIsOcupped = 
-            (binResult & ((ulong)table - (ulong)objectivePosition + 1)) == 0;
+        ulong binResult = CurrentPlays >>> objectivePosition - 1;
+        bool isOcupped = (CurrentPlays & ((ulong)1 << objectivePosition)) > 0; 
 
-
-        return objectiveIsOcupped;
+        return isOcupped;
     }
+
+    public void SearchPiece () {
+        ulong clone = CurrentPlays;
+
+        for(int i =0; i <= 63; i++){
+            bool index = ((clone >>> i) & 1) > 0;
+            if(index){
+                int x = (i / 8) + 1;
+                int y = (i % 8) + 1;
+                System.Console.WriteLine($"------Peça {i} x:{x}, y:{y}");
+                Plays(y,x);
+            }
+        }
+    }
+
 }
